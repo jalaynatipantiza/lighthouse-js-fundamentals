3d88842380518e8128199762dd4a1b13986a12d8
jalayna <jalaynatipantiza@gmail.com>
2020-05-08 14:37:07 +0000

    using functions for greetings

--- hello.js
+++ hello.js
@@ -1,6 +1,14 @@
+/*
 const sayHello = function(name){
 console.log("Hello " + name)
 }
 sayHello("Jalayna");
 sayHello("Landon")
 sayHello("Zaya")
+*/
+
+const sayHello = function(name){
+  return "Hello " + name;
+}
+const greetings = sayHello("Jay")
+console.log(greetings);
